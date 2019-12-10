1ea4697221033b47e10bc4c0a50647462c01f25d
Manoj Dhadke <manoj.dhadke@getflint.io>
2019-12-10 12:27:43 +0530

    Flint license header updated

--- hello.groovy
+++ hello.groovy
@@ -1,5 +1,19 @@
-// This is an first flintbit tutorial
-
+/*
+ *
+ * INFIVERVE TECHNOLOGIES PTE LIMITED CONFIDENTIAL
+ * _______________________________________________
+ *
+ *  (C) INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE
+ *  All Rights Reserved.
+ *  Product / Project: Flint IT Automation Platform
+ *  NOTICE:  All information contained herein is, and remains
+ *  the property of INFIVERVE TECHNOLOGIES PTE LIMITED.
+ *  The intellectual and technical concepts contained
+ *  herein are proprietary to INFIVERVE TECHNOLOGIES PTE LIMITED.
+ *  Dissemination of this information or any form of reproduction of this material
+ *  is strictly forbidden unless prior written permission is obtained
+ *  from INFIVERVE TECHNOLOGIES PTE LIMITED, SINGAPORE.
+ */
 log.info("Welcome to flint!")
 
 output.set("message","Hello World!")
